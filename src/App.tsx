89ce567cf018ca117755fd01ceec69aeefd3cb6a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-10 20:42:32 -0300

    feat: enhance form submission flow with conditional rendering and integrate InputPhone component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import { Box, Button, TextField } from '@mui/material';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { firstStepSchema } from './schemas/firstStepSchema';
+import InputPhone from './components/InputPhone';
 
 function App() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(firstStepSchema),
-    mode: 'onBlur',
+    mode: 'all',
   });
+
+  const firstName = watch('firstName');
+  const lastName = watch('lastName');
+  const birthDate = watch('birthDate');
+  const email = watch('email');
+  const phone = watch('phone');
 
   const onSubmit = (data: any) => {
     console.log('Form submitted:', data);
@@ -31,51 +39,74 @@ function App() {
             helperText={errors.firstName?.message?.toString()}
           />
         </Box>
-        <Box mb={2}>
-          <TextField
-            id="last-name"
-            label="Last Name"
-            fullWidth
-            {...register('lastName')}
-            error={!!errors.lastName}
-            helperText={errors.lastName?.message?.toString()}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            id="birth-date"
-            label="Birth Date"
-            fullWidth
-            type="date"
-            slotProps={{ inputLabel: { shrink: true } }}
-            {...register('birthDate')}
-            error={!!errors.birthDate}
-            helperText={errors.birthDate?.message?.toString()}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            id="email"
-            label="Email"
-            fullWidth
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors.email?.message?.toString()}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            id="phone"
-            label="Phone"
-            fullWidth
-            {...register('phone')}
-            error={!!errors.phone}
-            helperText={errors.phone?.message?.toString()}
-          />
-        </Box>
-        <Button variant="contained" color="primary" type="submit" fullWidth>
-          Next
-        </Button>
+        {firstName && !errors.firstName && (
+          <Box mb={2}>
+            <TextField
+              id="last-name"
+              label="Last Name"
+              fullWidth
+              {...register('lastName')}
+              error={!!errors.lastName}
+              helperText={errors.lastName?.message?.toString()}
+            />
+          </Box>
+        )}
+        {firstName && !errors.firstName && lastName && !errors.lastName && (
+          <Box mb={2}>
+            <TextField
+              id="birth-date"
+              label="Birth Date"
+              fullWidth
+              type="date"
+              slotProps={{ inputLabel: { shrink: true } }}
+              {...register('birthDate')}
+              error={!!errors.birthDate}
+              helperText={errors.birthDate?.message?.toString()}
+            />
+          </Box>
+        )}
+        {firstName &&
+          !errors.firstName &&
+          lastName &&
+          !errors.lastName &&
+          birthDate &&
+          !errors.birthDate && (
+            <Box mb={2}>
+              <TextField
+                id="email"
+                label="Email"
+                fullWidth
+                {...register('email')}
+                error={!!errors.email}
+                helperText={errors.email?.message?.toString()}
+              />
+            </Box>
+          )}
+        <InputPhone
+          shouldShow={
+            firstName &&
+            !errors.firstName &&
+            lastName &&
+            !errors.lastName &&
+            email &&
+            !errors.email
+          }
+          register={register}
+          errors={errors}
+        />
+
+        {firstName &&
+          !errors.firstName &&
+          lastName &&
+          !errors.lastName &&
+          email &&
+          !errors.email &&
+          phone &&
+          !errors.phone && (
+            <Button variant="contained" color="primary" type="submit" fullWidth>
+              Next
+            </Button>
+          )}
       </form>
     </Box>
   );
