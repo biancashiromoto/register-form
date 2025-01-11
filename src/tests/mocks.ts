32147dd40c76124f09956fb9d8e3177aedd881d4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 17:05:44 -0300

    feat: add mockCountries data for testing purposes

---
+++ src/tests/mocks.ts
@@ -0,0 +1,14 @@
+export const mockCountries = [
+  {
+    code: '+55',
+    nameEng: 'Brazil',
+    flag: { src: 'br-flag.png', altText: 'Brazil flag' },
+    iso: 'BRA',
+  },
+  {
+    code: '+1',
+    nameEng: 'Canada',
+    flag: { src: 'ca-flag.png', altText: 'Canada flag' },
+    iso: 'CAN',
+  },
+];
