5910af654c15cf4aa96323c4294bab05de2162f9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 18:41:53 -0300

    feat: add UserType interface to define user properties

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
+export interface UserType {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: Date;
+  phone: string;
+  countryCode: string;
+}
+
 export interface CountryType {
   code: string;
   nameEng: string;
