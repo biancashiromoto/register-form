3afc9e3ed26d02dd0dbb5778fba7440a2eb5853c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 17:01:03 -0300

    chore: add setup.ts to excluded files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,33 +1,34 @@
-import { defineConfig } from "vitest/config";
-import path from "path";
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.ts",
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
     css: true,
-    reporters: ["verbose"],
+    reporters: ['verbose'],
     coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
       exclude: [
-        "node_modules",
-        "dist",
-        "vite.config.ts",
-        "vitest.config.ts",
-        "eslint.config.cjs",
-        "**/*.d.ts",
-        "src/main.tsx",
-        "src/**/spec/*.spec.tsx",
-        "src/types/*.ts",
-        "src/context/index.types.ts",
-        "src/context/*",
+        'node_modules',
+        'dist',
+        'vite.config.ts',
+        'vitest.config.ts',
+        'eslint.config.cjs',
+        '**/*.d.ts',
+        'src/main.tsx',
+        'src/**/spec/*.spec.tsx',
+        'src/types/*.ts',
+        'src/context/index.types.ts',
+        'src/context/*',
+        'src/tests/setup.ts',
       ],
     },
   },
