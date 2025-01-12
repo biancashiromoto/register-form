14c10f8c078e35afd308cdc6a3ec5966157331b9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-12 00:57:33 -0300

    fix: update CountryCodeSelect to use phonecode and simplify country fetching

--- src/components/CountryCodeSelect/index.tsx
+++ src/components/CountryCodeSelect/index.tsx
@@ -1,9 +1,8 @@
+import { countries, filterCountries } from '@/helpers';
+import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import useFetchCountries from '@/hooks/useFetchCountries';
-import { CountryType } from '@/types';
-import { filterCountries } from '@/helpers';
+import { ICountry } from 'country-state-city';
 
 export type CountryCodeSelectProps = {
   setValue: (field: string, value: any) => void;
@@ -14,35 +13,28 @@ export default function CountryCodeSelect({
   setValue,
   errors,
 }: CountryCodeSelectProps) {
-  const { countries = [], isLoading, isFetching } = useFetchCountries();
-
-  const sortedCountries = [...countries].sort((a, b) =>
-    a.nameEng.localeCompare(b.nameEng, 'en', { sensitivity: 'base' }),
-  );
-
   return (
     <Autocomplete
-      loading={isLoading || isFetching}
       onChange={(_event, newValue) => {
-        setValue('countryCode', newValue?.code || '');
+        setValue('countryCode', newValue?.phonecode || '');
       }}
       id="country-code-select"
-      options={sortedCountries}
+      options={countries}
       autoHighlight
       groupBy={(option) =>
-        option.nameEng
+        option.name
           .charAt(0)
           .normalize('NFD')
           .replace(/[\u0300-\u036f]/g, '')
           .toUpperCase()
       }
       filterOptions={(options, state) => filterCountries({ options, state })}
-      getOptionLabel={(option: CountryType) => option.code}
+      getOptionLabel={(option: ICountry) => option.phonecode}
       renderOption={(props, option) => {
         const { key, ...optionProps } = props;
         return (
           <Box key={`${key}-${Math.random()}`} component="li" {...optionProps}>
-            {option.code} ({option.iso[0]})
+            {option.phonecode} ({option.isoCode})
           </Box>
         );
       }}
