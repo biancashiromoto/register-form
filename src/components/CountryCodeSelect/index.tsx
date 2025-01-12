456f219e429e84248e2239cc9540e01d5cecefb5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-12 01:42:28 -0300

    fix: refactor CountryCodeSelect component to use Select for improved rendering and simplified logic

--- src/components/CountryCodeSelect/index.tsx
+++ src/components/CountryCodeSelect/index.tsx
@@ -1,57 +1,29 @@
-import { countries, filterCountries } from '@/helpers';
-import Autocomplete from '@mui/material/Autocomplete';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { ICountry } from 'country-state-city';
+import { sortedCountries } from '@/helpers';
+import Select from '../Select';
 
 export type CountryCodeSelectProps = {
-  setValue: (field: string, value: any) => void;
+  setValue: any;
   errors: any;
+  shouldShow: boolean;
+  register: any;
 };
 
 export default function CountryCodeSelect({
   setValue,
   errors,
+  shouldShow,
+  register,
 }: CountryCodeSelectProps) {
   return (
-    <Autocomplete
-      onChange={(_event, newValue) => {
-        setValue('countryCode', newValue?.phonecode || '');
-      }}
-      id="country-code-select"
-      options={countries}
-      autoHighlight
-      groupBy={(option) =>
-        option.name
-          .charAt(0)
-          .normalize('NFD')
-          .replace(/[\u0300-\u036f]/g, '')
-          .toUpperCase()
-      }
-      filterOptions={(options, state) => filterCountries({ options, state })}
-      getOptionLabel={(option: ICountry) => option.phonecode}
-      renderOption={(props, option) => {
-        const { key, ...optionProps } = props;
-        return (
-          <Box key={`${key}-${Math.random()}`} component="li" {...optionProps}>
-            {option.phonecode} ({option.isoCode})
-          </Box>
-        );
-      }}
-      renderInput={(params) => (
-        <TextField
-          error={!!errors}
-          helperText={errors?.message}
-          {...params}
-          aria-label="Choose a country code"
-          slotProps={{
-            htmlInput: {
-              ...params.inputProps,
-              autoComplete: 'new-password',
-            },
-          }}
-        />
-      )}
+    <Select
+      register={register}
+      label="Choose a country code"
+      shouldShow={shouldShow}
+      setValue={setValue}
+      errors={errors}
+      name="countryCode"
+      options={sortedCountries}
+      displayedValue="phonecode"
     />
   );
 }
