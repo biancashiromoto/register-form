285393f7123e98128541a870de24d7434ca262ec
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 23:23:50 -0300

    feat: add CountrySelect component for selecting countries with autocomplete

---
+++ src/components/CountrySelect/index.tsx
@@ -0,0 +1,87 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import useFetchCountries from '@/hooks/useFetchCountries';
+import { CountryType } from '@/types';
+import { filterCountries } from '@/helpers';
+import { useState } from 'react';
+
+export type CountrySelectProps = {
+  setValue: any;
+  errors: any;
+  shouldShow: boolean;
+};
+
+export default function CountrySelect({
+  setValue,
+  errors,
+  shouldShow,
+}: CountrySelectProps) {
+  const { countries = [], isLoading, isFetching } = useFetchCountries();
+  const [selectedCountry, setSelectedCountry] = useState<CountryType | null>(
+    null,
+  );
+
+  const sortedCountries = [...countries].sort((a, b) =>
+    a.nameEng.localeCompare(b.nameEng, 'en', { sensitivity: 'base' }),
+  );
+
+  return (
+    shouldShow && (
+      <Autocomplete
+        loading={isLoading || isFetching}
+        onChange={(_event, newValue) => {
+          setSelectedCountry(newValue);
+          setValue('countryCode', newValue?.code || '');
+        }}
+        id="country-select"
+        options={sortedCountries}
+        autoHighlight
+        groupBy={(option) =>
+          option.nameEng
+            .charAt(0)
+            .normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .toUpperCase()
+        }
+        filterOptions={(options, state) => filterCountries({ options, state })}
+        getOptionLabel={(option: CountryType) => option.nameEng}
+        renderOption={(props, option) => {
+          const { key, ...optionProps } = props;
+          return (
+            <Box
+              key={`${key}-${Math.random()}`}
+              component="li"
+              {...optionProps}
+              sx={{ '& > img': { mr: 2, flexShrink: 5 } }}
+            >
+              <img
+                role="img"
+                loading="lazy"
+                width="20"
+                srcSet={option.flag?.src}
+                alt={option.flag?.altText || `${option.nameEng} flag`}
+                src={option.flag?.src}
+              />
+              {option.nameEng}
+            </Box>
+          );
+        }}
+        renderInput={(params) => (
+          <TextField
+            error={!!errors}
+            helperText={errors?.message}
+            {...params}
+            aria-label="Choose a country"
+            slotProps={{
+              htmlInput: {
+                ...params.inputProps,
+                autoComplete: 'new-password',
+              },
+            }}
+          />
+        )}
+      />
+    )
+  );
+}
