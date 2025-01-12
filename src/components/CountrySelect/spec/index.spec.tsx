360534fd2b788b27d033924cb692e149f5c62996
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 23:33:42 -0300

    test: update CountrySelect tests to verify country selection and filtering by name

--- src/components/CountrySelect/spec/index.spec.tsx
+++ src/components/CountrySelect/spec/index.spec.tsx
@@ -36,90 +36,37 @@ describe('CountrySelect', () => {
     });
   });
 
-  // it('calls setValue with the correct country code when an option is selected', () => {
-  //   renderComponent();
-  //   fireEvent.click(
-  //     screen.getByRole('button', {
-  //       name: /open/i,
-  //     }),
-  //   );
-  //   fireEvent.click(
-  //     screen.getByText(
-  //       `${mockCountries[0].code} (${mockCountries[0].nameEng})`,
-  //     ),
-  //   );
-  //   expect(mockSetValue).toHaveBeenCalledWith(
-  //     'countryCode',
-  //     mockCountries[0].code,
-  //   );
-  // });
+  it('calls setValue with the correct country code when an option is selected', () => {
+    renderComponent();
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /open/i,
+      }),
+    );
+    fireEvent.click(screen.getByText(mockCountries[0].nameEng));
+    expect(mockSetValue).toHaveBeenCalledWith(
+      'country',
+      mockCountries[0].nameEng,
+    );
+  });
 
-  // it('filters options based on country code', () => {
-  //   renderComponent();
-  //   fireEvent.click(
-  //     screen.getByRole('button', {
-  //       name: /open/i,
-  //     }),
-  //   );
-  //   fireEvent.change(screen.getByRole('combobox'), {
-  //     target: { value: mockCountries[0].code },
-  //   });
-  //   expect(
-  //     screen.queryByText(
-  //       `${mockCountries[1].code} (${mockCountries[1].nameEng})`,
-  //     ),
-  //   ).not.toBeInTheDocument();
-  //   expect(
-  //     screen.getByText(
-  //       `${mockCountries[0].code} (${mockCountries[0].nameEng})`,
-  //     ),
-  //   ).toBeInTheDocument();
-  // });
-
-  // it('filters options based on country name', () => {
-  //   renderComponent();
-  //   fireEvent.click(
-  //     screen.getByRole('button', {
-  //       name: /open/i,
-  //     }),
-  //   );
-  //   fireEvent.change(screen.getByRole('combobox'), {
-  //     target: { value: mockCountries[0].nameEng.slice(0, 3) },
-  //   });
-  //   expect(
-  //     screen.queryByText(
-  //       `${mockCountries[1].code} (${mockCountries[1].nameEng})`,
-  //     ),
-  //   ).not.toBeInTheDocument();
-  //   expect(
-  //     screen.getByText(
-  //       `${mockCountries[0].code} (${mockCountries[0].nameEng})`,
-  //     ),
-  //   ).toBeInTheDocument();
-  // });
-
-  // it('calls setValue with empty code if none is provided', () => {
-  //   const incompleteCountryObj: CountryType = {
-  //     code: '',
-  //     flag: {
-  //       altText: '',
-  //       src: 'japan.svg',
-  //     },
-  //     iso: 'JP',
-  //     nameEng: 'Japan',
-  //   };
-
-  //   renderComponent([...mockCountries, incompleteCountryObj]);
-  //   fireEvent.click(
-  //     screen.getByRole('button', {
-  //       name: /open/i,
-  //     }),
-  //   );
-  //   expect(screen.getAllByRole('img')[mockCountries.length]).toHaveAttribute(
-  //     'alt',
-  //     `${incompleteCountryObj.nameEng} flag`,
-  //   );
-  //   fireEvent.click(screen.getByText(`(${incompleteCountryObj.nameEng})`));
-  //   expect(mockSetValue).toHaveBeenCalledWith('countryCode', '');
-  // });
+  it('filters options based on country name', () => {
+    renderComponent();
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /open/i,
+      }),
+    );
+    fireEvent.change(screen.getByRole('combobox'), {
+      target: { value: mockCountries[0].nameEng.slice(0, 3) },
+    });
+    expect(
+      screen.queryByText(mockCountries[1].nameEng),
+    ).not.toBeInTheDocument();
+    fireEvent.click(screen.getByText(mockCountries[0].nameEng));
+    expect(mockSetValue).toHaveBeenCalledWith(
+      'country',
+      mockCountries[0].nameEng,
+    );
+  });
 });
