e1c9fc1c7e7606fc750fa269936f5cfc0a1c886e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-12 00:57:13 -0300

    fix: remove unnecessary QueryClient and PersistQueryClientProvider for cleaner rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,36 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { QueryClient } from '@tanstack/react-query';
-import {
-  Persister,
-  PersistQueryClientProvider,
-} from '@tanstack/react-query-persist-client';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import Provider from './context/Provider';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      gcTime: Infinity,
-    },
-  },
-});
-
-const persister: Persister = createSyncStoragePersister({
-  storage: window.localStorage,
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PersistQueryClientProvider
-      persistOptions={{ persister }}
-      client={queryClient}
-    >
-      <Provider>
-        <App />
-      </Provider>
-    </PersistQueryClientProvider>
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 );
