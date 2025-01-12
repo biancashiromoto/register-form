25b4207b691ea50d16a85d6dc323e85d20518e26
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-11 22:29:07 -0300

    test: refactor App component tests to use context provider and improve initial rendering checks

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,10 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
-import App from './App';
-import { mockCountries, mockUser } from './tests/mocks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import App from './App';
+import { Context, ContextProps } from './context';
 import useFetchCountries from './hooks/useFetchCountries';
+import { mockCountries } from './tests/mocks';
 
 vi.mock('@/hooks/useFetchCountries', () => {
   return {
@@ -12,6 +13,15 @@ vi.mock('@/hooks/useFetchCountries', () => {
 });
 
 const queryClient = new QueryClient();
+
+const mockFormStepsDispatch = vi.fn();
+
+const mockContext = {
+  formStepsDispatch: mockFormStepsDispatch,
+  formStepsState: {
+    activeStep: 0,
+  },
+} as unknown as ContextProps;
 
 describe('App Form', () => {
   beforeEach(() => {
@@ -24,68 +34,19 @@ describe('App Form', () => {
     });
     render(
       <QueryClientProvider client={queryClient}>
-        <App />
+        <Context.Provider value={mockContext}>
+          <App />
+        </Context.Provider>
       </QueryClientProvider>,
     );
   });
 
   it('renders the initial form', () => {
-    expect(screen.getByText(/register form/i)).toBeInTheDocument();
-
-    const firstNameInput = screen.getByRole('textbox', {
-      name: /first name/i,
-    });
-    expect(firstNameInput).toBeInTheDocument();
-
-    const lastNameInput = screen.queryByRole('textbox', {
-      name: /last name/i,
-    });
-    expect(lastNameInput).not.toBeInTheDocument();
-
-    const nextButton = screen.queryByRole('button', { name: /next/i });
-    expect(nextButton).not.toBeInTheDocument();
-  });
-
-  it('handles correct form filling', async () => {
-    fireEvent.change(
-      screen.getByRole('textbox', {
-        name: /first name/i,
-      }),
-      { target: { value: mockUser.firstName } },
-    );
-
-    fireEvent.change(
-      screen.getByRole('textbox', {
-        name: /last name/i,
-      }),
-      { target: { value: mockUser.lastName } },
-    );
-
-    fireEvent.change(screen.getByLabelText(/birth date/i), {
-      target: { value: mockUser.birthDate },
-    });
-
-    fireEvent.change(
-      screen.getByRole('textbox', {
-        name: /e-mail/i,
-      }),
-      { target: { value: mockUser.email } },
-    );
-
-    fireEvent.click(screen.getByRole('button', { name: /open/i }));
-    await screen.findByText(
-      `${mockCountries[0].code} (${mockCountries[0].nameEng})`,
-    );
-    fireEvent.click(
-      screen.getByText(
-        `${mockCountries[0].code} (${mockCountries[0].nameEng})`,
-      ),
-    );
-
-    fireEvent.change(screen.getByRole('textbox', { name: /phone/i }), {
-      target: { value: mockUser.phone },
-    });
-
-    expect(screen.getByRole('button', { name: /next/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /register form/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /step 1: user register/i }),
+    ).toBeInTheDocument();
   });
 });
