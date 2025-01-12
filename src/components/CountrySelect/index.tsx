6ee8b5172c47b06f1beedffa4843cb84b974c74f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-12 01:42:49 -0300

    fix: refactor CountrySelect component to use Select for improved rendering and simplified logic

--- src/components/CountrySelect/index.tsx
+++ src/components/CountrySelect/index.tsx
@@ -1,8 +1,5 @@
-import { countries, filterCountries } from '@/helpers';
-import Autocomplete from '@mui/material/Autocomplete';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { ICountry } from 'country-state-city';
+import { sortedCountries } from '@/helpers';
+import Select from '../Select';
 
 export type CountrySelectProps = {
   setValue: any;
@@ -18,45 +15,15 @@ export default function CountrySelect({
   register,
 }: CountrySelectProps) {
   return (
-    shouldShow && (
-      <Autocomplete
-        onChange={(_event, newValue) => {
-          setValue('country', newValue?.name || '');
-        }}
-        id="country-select"
-        options={countries}
-        autoHighlight
-        groupBy={(option) => option.name[0]}
-        filterOptions={(options, state) => filterCountries({ options, state })}
-        getOptionLabel={(option: ICountry) => option.name}
-        renderOption={(props, option) => {
-          const { key, ...optionProps } = props;
-          return (
-            <Box
-              key={`${key}-${Math.random()}`}
-              component="li"
-              {...optionProps}
-            >
-              {option.flag} {option.name}
-            </Box>
-          );
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...register('country')}
-            error={!!errors.country}
-            helperText={errors?.message}
-            {...params}
-            label="Choose a country"
-            slotProps={{
-              htmlInput: {
-                ...params.inputProps,
-                autoComplete: 'new-password',
-              },
-            }}
-          />
-        )}
-      />
-    )
+    <Select
+      register={register}
+      label="Choose a country"
+      shouldShow={shouldShow}
+      setValue={setValue}
+      errors={errors}
+      name="country"
+      options={sortedCountries}
+      displayedValue="name"
+    />
   );
 }
