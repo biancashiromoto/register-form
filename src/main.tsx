0a24ae96c7090abb235bbf52573247f91ba24a32
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-29 20:26:17 -0300

    feat: integrate React Query and context provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+import Provider from './context/Provider';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      gcTime: Infinity,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
