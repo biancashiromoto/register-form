c23dd33205ca4a05ff435e2f90bbc57e9298d452
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-29 20:53:37 -0300

    feat: enhance Snackbar component to use Alert for severity display and improve close handling

--- src/components/Snackbar/index.tsx
+++ src/components/Snackbar/index.tsx
@@ -1,26 +1,39 @@
 import { Context } from '@/context';
-import { Snackbar as MuiSnackbar } from '@mui/material';
+import { Alert, Snackbar as MuiSnackbar } from '@mui/material';
 import { useContext } from 'react';
 
 export const CustomSnackbar = () => {
   const { setSnackbarState, snackBarState } = useContext(Context);
+
+  const handleClose = () => {
+    setSnackbarState((prevState) => {
+      return {
+        ...prevState,
+        open: false,
+      };
+    });
+  };
+
+  if (!snackBarState) return null;
+
   return (
     <MuiSnackbar
-      open={snackBarState.open}
+      open={snackBarState.open || false}
       autoHideDuration={5000}
-      onClose={() =>
-        setSnackbarState((prevState) => {
-          return {
-            ...prevState,
-            open: false,
-          };
-        })
-      }
-      message={snackBarState.message}
+      onClose={handleClose}
       anchorOrigin={{
         vertical: 'top',
         horizontal: 'right',
       }}
-    />
+    >
+      <Alert
+        onClose={handleClose}
+        severity={snackBarState.severity}
+        variant="filled"
+        sx={{ width: '100%' }}
+      >
+        {snackBarState.message}
+      </Alert>
+    </MuiSnackbar>
   );
 };
