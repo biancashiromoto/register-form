46d309b75023a6ab22c297127d9db26dbf42fe25
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-29 19:30:11 -0300

    feat: add environment variable definitions for VITE_PROJECT_URL and VITE_SUPABASE_KEY in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from "path";
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  define: {
+    'process.env.VITE_PROJECT_URL': JSON.stringify(
+      process.env.VITE_PROJECT_URL,
+    ),
+    'process.env.VITE_SUPABASE_KEY': JSON.stringify(
+      process.env.VITE_SUPABASE_KEY,
+    ),
   },
 });
