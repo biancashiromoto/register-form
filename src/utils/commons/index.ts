8296ea9d4c690a75c2e410a1e8208e67e8919509
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-01-29 20:52:47 -0300

    feat: add INITIAL_USER_STATE for user management

---
+++ src/utils/commons/index.ts
@@ -0,0 +1,10 @@
+import { UserType } from '@/types';
+
+export const INITIAL_USER_STATE: UserType = {
+  firstName: '',
+  lastName: '',
+  birthDate: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
