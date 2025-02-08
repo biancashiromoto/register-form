a4a90c0cc5c6131d7592eebf2c32d45e32d73c34
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-08 15:29:32 -0300

    feat: add Footer component with GitHub link

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+import { IoLogoGithub, IoOpenOutline } from 'react-icons/io5';
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        position: 'fixed',
+        bottom: '0',
+        borderTop: 'solid 1px gray',
+        width: '100%',
+        padding: '12px 6px',
+      }}
+    >
+      <a
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/biancashiromoto/register-form"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <IoLogoGithub
+          size={25}
+          aria-label="GitHub repo"
+          style={{ paddingRight: '6px' }}
+        />
+        GitHub repo <IoOpenOutline aria-label="Opens in a new tab" />
+      </a>{' '}
+    </footer>
+  );
+};
+
+export default Footer;
