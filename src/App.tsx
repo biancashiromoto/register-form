4d45701a294c9e1c7a0799c94f34c1d866f8812a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-11 19:53:29 -0300

    feat: wrap title in header for improved semantic structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <h1>Project Register Form</h1>
+      <header>
+        <h1>Project Register Form</h1>
+      </header>
       <Footer />
     </>
   );
