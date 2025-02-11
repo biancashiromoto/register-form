0f71a48338557c46529bf6ec3409ffdb703f5686
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-11 19:52:28 -0300

    feat: add user state management to context props

--- src/context/index.types.ts
+++ src/context/index.types.ts
@@ -1,7 +1,9 @@
-import { SnackbarStateType } from '@/types';
+import { AddressType, SnackbarStateType, UserType } from '@/types';
 import { Dispatch, SetStateAction } from 'react';
 
 export interface ContextProps {
   snackbarState: SnackbarStateType;
   setSnackbarState: Dispatch<SetStateAction<SnackbarStateType>>;
+  user: UserType | null;
+  setUser: Dispatch<SetStateAction<UserType | null>>;
 }
