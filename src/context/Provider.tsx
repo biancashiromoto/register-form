f1c0531a9129d3f74ca04418bb538efcca534d65
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-11 19:52:19 -0300

    feat: add user state management to context provider

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,4 +1,4 @@
-import { SnackbarStateType } from '@/types';
+import { AddressType, SnackbarStateType, UserType } from '@/types';
 import { FC, ReactNode, useState } from 'react';
 import { Context } from '.';
 import { ContextProps } from './index.types';
@@ -9,10 +9,13 @@ const Provider: FC<{ children: ReactNode }> = ({ children }) => {
     message: '',
     severity: undefined,
   });
+  const [user, setUser] = useState<UserType | null>(null);
 
   const value: ContextProps = {
     snackbarState,
     setSnackbarState,
+    user,
+    setUser,
   };
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
