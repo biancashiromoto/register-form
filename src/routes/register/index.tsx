bda45fd40bf7ed554f501f23e8249daa7233aace
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-11 19:50:44 -0300

    feat: remove RouteComponent and navigation links from registration route

--- src/routes/register/index.tsx
+++ src/routes/register/index.tsx
@@ -1,28 +1,10 @@
-import { createRoute, Link, Outlet, redirect } from '@tanstack/react-router';
+import { createRoute, redirect } from '@tanstack/react-router';
 import { Route as RootRoute } from '../__root';
 
 export const Route = createRoute({
   getParentRoute: () => RootRoute,
   path: '/register',
-  component: RouteComponent,
   loader: async () => {
     return redirect({ to: '/register/user' });
   },
 });
-
-function RouteComponent() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <nav>
-        <Link to="/register/user" className="[&.active]:font-bold">
-          User
-        </Link>
-        <Link to="/register/address" className="[&.active]:font-bold">
-          Address
-        </Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
-}
