aa04579f362c7d91567fcea9cedffa2cc4203bbd
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-14 19:23:50 -0300

    feat: update secondStepSchema to use structured objects for country, state, and city validation

--- src/schemas/secondStepSchema.ts
+++ src/schemas/secondStepSchema.ts
@@ -1,13 +1,16 @@
 import { z } from 'zod';
 
+const countryAndStateSchema = z.object({
+  name: z.string(),
+  isoCode: z.string(),
+});
+
+const citySchema = z.object({
+  name: z.string(),
+});
+
 export const secondStepSchema = z.object({
-  country: z.string({
-    required_error: 'Country is required',
-  }),
-  state: z.string({
-    required_error: 'State is required',
-  }),
-  city: z.string({
-    required_error: 'City is required',
-  }),
+  country: countryAndStateSchema,
+  state: countryAndStateSchema,
+  city: citySchema,
 });
