32674e6c0107f22689dce77eca4c446cc4affa41
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-02-14 19:23:30 -0300

    feat: add selectedLocation and setSelectedLocation state with useEffect logging

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,5 +1,5 @@
 import { AddressType, SnackbarStateType, UserType } from '@/types';
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import { Context } from '.';
 import { ContextProps } from './index.types';
 
@@ -10,12 +10,15 @@ const Provider: FC<{ children: ReactNode }> = ({ children }) => {
     severity: undefined,
   });
   const [user, setUser] = useState<UserType | null>(null);
+  const [selectedLocation, setSelectedLocation] = useState({} as AddressType);
 
   const value: ContextProps = {
     snackbarState,
     setSnackbarState,
     user,
     setUser,
+    selectedLocation,
+    setSelectedLocation,
   };
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
