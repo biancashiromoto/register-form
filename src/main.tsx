07be38cfa28525c3e0cf45c88e78dbc4a6f47b11
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-05 19:45:08 -0300

    feat: integrate browser history into router configuration for improved navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import './index.css';
 import Provider from './context/Provider';
 import { routeTree } from './routeTree.gen';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import {
+  createBrowserHistory,
+  createRouter,
+  RouterProvider,
+} from '@tanstack/react-router';
 
-const router = createRouter({ routeTree, basepath: '/' });
+const history = createBrowserHistory({
+  createHref: (path: string) => path,
+});
+const router = createRouter({ routeTree, history });
 
 declare module '@tanstack/react-router' {
   interface Register {
