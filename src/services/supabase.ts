7a87778c0e23a7c8c6f1836e962c1da895850780
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-05 19:56:35 -0300

    feat: log SUPABASE_URL and SUPABASE_KEY for debugging purposes

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 export const SUPABASE_URL = import.meta.env.VITE_PROJECT_URL;
 export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
+console.log('SUPABASE_URL:', import.meta.env.VITE_PROJECT_URL);
+console.log('SUPABASE_KEY:', import.meta.env.VITE_SUPABASE_KEY);
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
