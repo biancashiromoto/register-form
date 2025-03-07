b4e95278a7fb5923730bd75c6ecefe436591c189
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-07 19:05:33 -0300

    feat: add custom hook for user login with error handling and navigation

---
+++ src/hooks/useLoginUser.tsx
@@ -0,0 +1,46 @@
+import { useMutation } from '@tanstack/react-query';
+import { loginUser } from '@/services/user';
+import { useNavigate } from '@tanstack/react-router';
+import { UseFormSetError } from 'react-hook-form';
+
+interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+type SetErrorFunction = UseFormSetError<{ email: string; password: string }>;
+
+const useLoginUser = (setError: SetErrorFunction) => {
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationKey: ['loginUser'],
+    mutationFn: async (credentials: LoginCredentials) => {
+      const response = await loginUser(credentials);
+      if (!response || !response.data?.session) {
+        throw new Error('Invalid login credentials');
+      }
+      return response.data;
+    },
+    onSuccess: () => {
+      navigate({ to: '/home' });
+    },
+    onError: (error: any) => {
+      setError('email', {
+        type: 'manual',
+        message: error.message || 'Incorrect email or password',
+      });
+      setError('password', {
+        type: 'manual',
+        message: error.message || 'Incorrect email or password',
+      });
+    },
+  });
+
+  return {
+    login: mutation.mutate,
+    isError: mutation.isError,
+  };
+};
+
+export default useLoginUser;
