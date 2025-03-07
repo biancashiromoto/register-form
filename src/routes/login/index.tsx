19259c79b013c4906b1c2fa45a239c73764f3e92
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-07 19:05:40 -0300

    feat: refactor login route to use custom hook for user authentication

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,11 +1,11 @@
 import InputPassword from '@/components/InputPassword';
 import InputText from '@/components/InputText';
+import useLoginUser from '@/hooks/useLoginUser';
 import { useResetForm } from '@/hooks/useResetForm';
 import { loginSchema } from '@/schemas/loginSchema';
-import { loginUser } from '@/services/user';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Box, Button } from '@mui/material';
-import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
 
 export const Route = createFileRoute('/login/')({
@@ -32,27 +32,10 @@ function RouteComponent() {
   const email = watch('email');
   const password = watch('password');
   useResetForm(email, resetField, 'password');
-  const navigate = useNavigate();
+  const { login } = useLoginUser(setError);
 
-  const onSubmit = async () => {
-    try {
-      const response = await loginUser({ email, password });
-      if (response) {
-        const { data } = response;
-        if (data.session['access_token']) {
-          navigate({ to: '/home' });
-        }
-      }
-    } catch (error: any) {
-      setError('email', {
-        type: 'manual',
-        message: 'Incorrect email or password',
-      });
-      setError('password', {
-        type: 'manual',
-        message: 'Incorrect email or password',
-      });
-    }
+  const onSubmit = (data: { email: string; password: string }) => {
+    login(data);
   };
 
   return (
