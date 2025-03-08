cf3c045807bdd4512ce0a7a43973101277b66a09
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-08 18:52:43 -0300

    feat: update VerificationLayout to use currentSession and handle loading state

--- src/components/VerificationLayout/index.tsx
+++ src/components/VerificationLayout/index.tsx
@@ -9,18 +9,24 @@ import { FC, ReactNode } from 'react';
 const VerificationLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { user, initializing } = useAuth();
+  const { currentSession, initializing } = useAuth();
   const publicRoutes = ['/login', '/unauthenticated'];
   const isRegisterRoute = location.pathname.includes('/register');
   const isPublicRoute =
-    publicRoutes.includes(location.pathname) || isRegisterRoute;
+    publicRoutes.includes(location.pathname) ||
+    isRegisterRoute ||
+    location.pathname === '/';
 
   useLayoutEffect(() => {
-    if (location.pathname === '/' || initializing) return;
-    if (!user && !isPublicRoute) {
+    if (initializing) return;
+    if (!currentSession && !isPublicRoute) {
       navigate({ to: '/unauthenticated', replace: true });
     }
-  }, [user, location.pathname, navigate]);
+  }, [location.pathname, currentSession, initializing, isPublicRoute]);
+
+  if (initializing) {
+    return <div>Loading...</div>;
+  }
 
   return <div>{children}</div>;
 };
