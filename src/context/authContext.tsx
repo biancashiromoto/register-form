25d545ca3401078730fed5a67cdc69dc5d9d030d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-08 17:01:04 -0300

    feat: enhance AuthContext to include user initialization state

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,24 +1,51 @@
 import { supabase } from '@/services/supabase';
 import { User } from '@supabase/supabase-js';
+import { ReactNode } from '@tanstack/react-router';
 import { createContext, useContext, useEffect, useState } from 'react';
 
-const AuthContext = createContext({} as any);
+interface AuthContextType {
+  user: User | null;
+  setUser: (user: User) => void;
+  initializing: boolean;
+}
 
-export const AuthProvider = ({ children }: any) => {
+const AuthContext = createContext({} as AuthContextType);
+
+export const AuthProvider = ({ children }: ReactNode) => {
   const [user, setUser] = useState<User | null>(null);
+  const [initializing, setInitializing] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
-      user && setUser(user);
+
+      if (user) {
+        setUser(user);
+        setInitializing(false);
+      }
     };
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session?.user ?? null);
+        setInitializing(false);
+      },
+    );
+
     fetchUser();
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
   }, []);
 
+  useEffect(() => {
+    console.log('user: ', user);
+  }, [user]);
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, initializing }}>
       {children}
     </AuthContext.Provider>
   );
