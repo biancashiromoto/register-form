09bb8b46bd4cc1ed34800406b9e5621b2698cba7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-08 20:36:39 -0300

    test: add unit tests for Footer component

---
+++ src/components/Footer/spec/index.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Footer from '..';
+
+describe('Footer Component', () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  it('renders a link with the correct attributes', () => {
+    const linkElement = screen.getByRole('link', { name: /GitHub repo/i });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute(
+      'href',
+      'https://github.com/biancashiromoto/register-form',
+    );
+    expect(linkElement).toHaveAttribute('target', '_blank');
+    expect(linkElement).toHaveAttribute('rel', 'noopener');
+  });
+
+  it('renders the GitHub icon with proper aria-label', () => {
+    const githubIcon = screen.getByLabelText('GitHub repo');
+    expect(githubIcon).toBeInTheDocument();
+  });
+
+  it('renders the open icon with proper aria-label', () => {
+    const openIcon = screen.getByLabelText('Opens in a new tab');
+    expect(openIcon).toBeInTheDocument();
+  });
+});
