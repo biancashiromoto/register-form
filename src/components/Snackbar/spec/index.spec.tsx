33aab0f74d7c80cd45093fd8c28b836584c78545
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-08 21:12:27 -0300

    test: add unit tests for CustomSnackbar component with onCloseCallback functionality

--- src/components/Snackbar/spec/index.spec.tsx
+++ src/components/Snackbar/spec/index.spec.tsx
@@ -1,12 +1,19 @@
-import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { CustomSnackbar } from '..';
 import { Context } from '@/context';
 import { ContextProps } from '@/context/index.types';
-import { SnackbarStateType } from '@/types';
+import { AddressType, UserType } from '@/types';
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { ICity, ICountry, IState } from 'country-state-city';
+import { SetStateAction } from 'react';
+import { CustomSnackbar } from '..';
 
 describe('CustomSnackbar', () => {
   const mockSetSnackbarState = vi.fn();
+  const mockSelectedLocation = {
+    country: {} as ICountry,
+    state: {} as IState,
+    city: {} as ICity,
+  };
 
   const mockContext = {
     setSnackbarState: mockSetSnackbarState,
@@ -17,6 +24,8 @@ describe('CustomSnackbar', () => {
     },
   } as unknown as ContextProps;
 
+  const mockOnCloseCallback = vi.fn();
+
   beforeEach(() => {
     vi.clearAllMocks();
   });
@@ -24,12 +33,12 @@ describe('CustomSnackbar', () => {
   const renderComponent = (context = mockContext) => {
     render(
       <Context.Provider value={context}>
-        <CustomSnackbar />
+        <CustomSnackbar onCloseCallback={mockOnCloseCallback} />
       </Context.Provider>,
     );
   };
 
-  it('should render snackbar if openSnackbar is true', async () => {
+  it('should render snackbar if openSnackbar is true and close after click on close button', async () => {
     renderComponent();
 
     await waitFor(() => {
@@ -44,6 +53,7 @@ describe('CustomSnackbar', () => {
       }),
     );
     expect(mockSetSnackbarState).toHaveBeenCalledTimes(1);
+    expect(mockOnCloseCallback).toHaveBeenCalledTimes(1);
   });
 
   it('should not render snackbar if openSnackbar is false', () => {
@@ -53,6 +63,16 @@ describe('CustomSnackbar', () => {
         open: false,
         message: 'Test message',
         severity: 'success',
+      },
+      registeringUser: null,
+      setRegisteringUser: function (
+        value: SetStateAction<UserType | null>,
+      ): void {
+        throw new Error('Function not implemented.');
+      },
+      selectedLocation: mockSelectedLocation,
+      setSelectedLocation: function (value: SetStateAction<AddressType>): void {
+        throw new Error('Function not implemented.');
       },
     });
 
