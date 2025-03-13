e850dc9f62df3f984f5383fe4dbac01199850569
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-13 20:35:52 -0300

    refactor: remove ThemeProvider and CssBaseline from App component for simplified structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { useContext, useMemo } from 'react';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import VerificationLayout from './components/VerificationLayout';
-import { Context } from './context';
 import Header from './components/Header';
+import VerificationLayout from './components/VerificationLayout';
 
 function App() {
-  const { theme } = useContext(Context);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <VerificationLayout>
-        <Header />
-        <Footer />
-      </VerificationLayout>
-    </ThemeProvider>
+    <VerificationLayout>
+      <Header />
+      <Footer />
+    </VerificationLayout>
   );
 }
 
