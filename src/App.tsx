0cbc2f001d5b4bd17bfacdf7eabfd38c0aba574c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-13 19:51:36 -0300

    feat: integrate ThemeProvider and CssBaseline in App component for theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { useContext, useMemo } from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import VerificationLayout from './components/VerificationLayout';
+import { Context } from './context';
+import Header from './components/Header';
 
 function App() {
+  const { theme } = useContext(Context);
+
   return (
-    <VerificationLayout>
-      <Navbar />
-      <Footer />
-    </VerificationLayout>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <VerificationLayout>
+        <Header />
+        <Footer />
+      </VerificationLayout>
+    </ThemeProvider>
   );
 }
 
