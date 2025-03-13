4bc3c61f344ec04732e0aebe57dad9575966785a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-13 19:51:16 -0300

    feat: update InputText component to support hidden prop and extend input attributes

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -2,47 +2,49 @@ import { useAuth } from '@/context/authContext';
 import { labels } from '@/helpers/labels';
 import { UserType } from '@/types';
 import { Box, TextField } from '@mui/material';
+import { ComponentProps } from 'react';
 import { FieldErrors } from 'react-hook-form';
 
 type LabelKeys = keyof typeof labels;
 
-export type InputTextProps = {
-  shouldShow?: boolean;
+export interface InputTextProps extends ComponentProps<'input'> {
   name: LabelKeys;
   register: any;
   errors: FieldErrors<UserType>;
   required?: boolean;
   autoComplete?: string;
-};
+}
 
 const InputText = ({
-  shouldShow = true,
   errors,
   register,
   name,
   required = false,
   autoComplete = '',
+  hidden,
   ...rest
 }: InputTextProps) => {
   const { user } = useAuth();
 
+  if (hidden) {
+    return null;
+  }
+
   return (
-    shouldShow && (
-      <Box mb={2}>
-        <TextField
-          id={name}
-          label={labels[name]}
-          fullWidth
-          {...register(name)}
-          error={!!errors[name]}
-          helperText={errors[name]?.message?.toString()}
-          required={required}
-          autoComplete={autoComplete}
-          {...rest}
-          defaultValue={user?.user_metadata[name]}
-        />
-      </Box>
-    )
+    <Box mb={2}>
+      <TextField
+        id={name}
+        label={labels[name]}
+        fullWidth
+        {...register(name)}
+        error={!!errors[name]}
+        helperText={errors[name]?.message?.toString()}
+        required={required}
+        autoComplete={autoComplete}
+        defaultValue={user?.user_metadata[name]}
+        {...rest}
+      />
+    </Box>
   );
 };
 
