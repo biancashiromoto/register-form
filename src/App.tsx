c2988f7e0d675cefde87fbf3f8c48c10089ff4db
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 17:45:39 -0300

    feat: wrap VerificationLayout in Container for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Container } from '@mui/material';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import VerificationLayout from './components/VerificationLayout';
 
 function App() {
   return (
-    <VerificationLayout>
-      <Header />
-      <Footer />
-    </VerificationLayout>
+    <Container maxWidth="sm">
+      <VerificationLayout>
+        <Header />
+        <Footer />
+      </VerificationLayout>
+    </Container>
   );
 }
 
