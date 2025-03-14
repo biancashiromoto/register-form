95203a7dd0b47f9fc31c37716a98782dea6b8ca4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 19:55:12 -0300

    feat: integrate usePageTitle hook and display page title in RootLayout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,29 @@
 import App from '@/App';
 import { Context } from '@/context';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import usePageTitle from '@/hooks/usePageTitle';
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { useContext } from 'react';
-
 const RootLayout = () => {
   const { theme } = useContext(Context);
+  const { page } = usePageTitle();
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
       <hr />
+      <Container maxWidth="sm" sx={{ marginTop: '16px' }}>
+        <Typography variant="h5" align="left">
+          {page.title}
+        </Typography>
+      </Container>
       <Outlet />
       {process.env.NODE_ENV === 'development' && <TanStackRouterDevtools />}
     </ThemeProvider>
