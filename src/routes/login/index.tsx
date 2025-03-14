7dadeeed925bf0a2fbc27da6d9ace1aa1c74e641
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 16:10:34 -0300

    feat: replace MUI Button with CustomButton in login form and update onSubmit data type

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,10 +1,12 @@
+import CustomButton from '@/components/Button';
 import InputPassword from '@/components/InputPassword';
 import InputText from '@/components/InputText';
 import useLoginUser from '@/hooks/useLoginUser';
 import { useResetForm } from '@/hooks/useResetForm';
 import { loginSchema } from '@/schemas/loginSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
+import { SignInWithPasswordCredentials } from '@supabase/supabase-js';
 import { createFileRoute } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
 
@@ -34,48 +36,42 @@ function RouteComponent() {
   useResetForm(email, resetField, 'password');
   const { login } = useLoginUser(setError);
 
-  const onSubmit = (data: { email: string; password: string }) => {
+  const onSubmit = (data: SignInWithPasswordCredentials) => {
     login(data);
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <Box
-        width="100%"
-        maxWidth="400px"
-        mx="auto"
-        mt={4}
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <InputText
-          errors={errors}
-          name="email"
-          register={register}
-          required
-          autoComplete="email"
-        />
+    <Box
+      mt={2}
+      mx={2}
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      gap={2}
+    >
+      <InputText
+        errors={errors}
+        name="email"
+        register={register}
+        required
+        autoComplete="email"
+      />
 
-        <InputPassword
-          shouldShow={!!email}
-          errors={errors}
-          register={register}
-          isExistingPassword
-        />
+      <InputPassword
+        hidden={!!errors.email}
+        errors={errors}
+        register={register}
+        isExistingPassword
+      />
 
-        {!!email && !errors.email && !!password && !errors.password && (
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            fullWidth
-            style={{ marginTop: '24px' }}
-          >
-            Next
-          </Button>
-        )}
-      </Box>
-    </div>
+      {!!email && !errors.email && !!password && !errors.password && (
+        <CustomButton variant="contained" color="primary" type="submit">
+          Next
+        </CustomButton>
+      )}
+    </Box>
   );
 }
