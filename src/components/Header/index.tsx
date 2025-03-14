a3fa2f86df6278081425e48c39f7e0aa1045e7a3
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 19:09:54 -0300

    feat: replace CustomSwitch with ToggleThemeSwitch and wrap Header in Container for improved layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import { useContext } from 'react';
 import Navbar from '../Navbar';
-import { CustomSwitch } from '../Switch';
+import { ToggleThemeSwitch } from '../ToggleThemeSwitch';
 import { Context } from '@/context';
+import { Container } from '@mui/material';
 
 const Header = () => {
   const { toggleTheme, isDarkModeOn } = useContext(Context);
 
   return (
-    <header style={{ display: 'flex', alignItems: 'center', height: '3em' }}>
-      <CustomSwitch onChange={toggleTheme} checked={isDarkModeOn} />
-      <Navbar style={{ position: 'absolute', right: '12px' }} />
-    </header>
+    <Container>
+      <header style={{ display: 'flex', alignItems: 'center', height: '3em' }}>
+        <ToggleThemeSwitch onChange={toggleTheme} checked={isDarkModeOn} />
+        <Navbar style={{ position: 'absolute', right: '12px' }} />
+      </header>
+    </Container>
   );
 };
 
