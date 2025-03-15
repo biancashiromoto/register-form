0788ebbeac4bdbd1968d4f0fe0633724a0c955a7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 23:21:19 -0300

    refactor: remove App component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,6 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import VerificationLayout from './components/VerificationLayout';
 
-function App() {
-  return (
-    <Container maxWidth="sm">
-      <VerificationLayout>
-        <Header />
-        <Footer />
-      </VerificationLayout>
-    </Container>
-  );
-}
+function App() {}
 
 export default App;
