8fbfc5e6c3e5a38a96814823928c884ba1158320
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 23:23:58 -0300

    refactor: replace header element with Box component in Header for improved styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,20 @@ import { useContext } from 'react';
 import Navbar from '../Navbar';
 import { ToggleThemeSwitch } from '../ToggleThemeSwitch';
 import { Context } from '@/context';
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 const Header = () => {
   const { toggleTheme, isDarkModeOn } = useContext(Context);
 
   return (
     <Container>
-      <header style={{ display: 'flex', alignItems: 'center', height: '3em' }}>
+      <Box
+        component="header"
+        style={{ display: 'flex', alignItems: 'center', height: '3em' }}
+      >
         <ToggleThemeSwitch onChange={toggleTheme} checked={isDarkModeOn} />
         <Navbar style={{ position: 'absolute', right: '12px' }} />
-      </header>
+      </Box>
     </Container>
   );
 };
