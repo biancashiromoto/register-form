f6457c2737b4ec4e0e83a5cc0cf38b56886a13d3
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-14 21:16:39 -0300

    fix: make handleClose prop optional in LoadingLayer component

--- src/components/LoadingLayer/index.tsx
+++ src/components/LoadingLayer/index.tsx
@@ -2,7 +2,7 @@ import { Backdrop, CircularProgress } from '@mui/material';
 
 export interface LoadingLayerProps {
   open: boolean;
-  handleClose: () => void;
+  handleClose?: () => void;
 }
 
 const LoadingLayer = ({ handleClose, open }: LoadingLayerProps) => {
@@ -10,7 +10,7 @@ const LoadingLayer = ({ handleClose, open }: LoadingLayerProps) => {
     <Backdrop
       sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
       open={open}
-      onClick={handleClose}
+      onClick={handleClose || undefined}
     >
       <CircularProgress data-testid="loading-img" color="inherit" />
     </Backdrop>
