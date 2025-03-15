2a671f6b644527c1b27a272d14cd7b572b846717
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-15 15:39:39 -0300

    feat: add CustomBottomNavigation component for improved user navigation

---
+++ src/components/BottomNavigation/index.tsx
@@ -0,0 +1,45 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import { useNavigate } from '@tanstack/react-router';
+import { useRef, useState } from 'react';
+
+export default function CustomBottomNavigation() {
+  const [value, setValue] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+      <CssBaseline />
+      <Paper
+        sx={{ position: 'fixed', bottom: 36, left: 0, right: 0, marginTop: 6 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Profile"
+            icon={<AccountCircleIcon />}
+            onClick={() => navigate({ to: '/profile' })}
+          />
+          {/* <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} /> */}
+          <BottomNavigationAction
+            label="Home"
+            icon={<HomeIcon />}
+            onClick={() => navigate({ to: '/home' })}
+          />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
