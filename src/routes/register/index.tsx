73f0c92ed1ad80c5394fea40cd0c24812fb460bd
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-16 18:04:20 -0300

    refactor: import useLayoutEffect from react-router for improved layout handling

--- src/routes/register/index.tsx
+++ src/routes/register/index.tsx
@@ -14,6 +14,7 @@ import { SnackbarStateType, UserType } from '@/types';
 import { INITIAL_USER_STATE } from '@/utils/commons';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Box, Container } from '@mui/material';
+import { useLayoutEffect } from '@tanstack/react-router';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import {
   City,
