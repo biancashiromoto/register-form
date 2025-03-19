05a4b5e8710ecd0edf618739981598ce6f9731a1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-19 20:09:57 -0300

    feat: simplify ContextProps by removing unused user and location states

--- src/context/index.types.ts
+++ src/context/index.types.ts
@@ -1,14 +1,10 @@
-import { AddressType, SnackbarStateType, UserType } from '@/types';
+import { SnackbarStateType } from '@/types';
 import { Theme } from '@mui/material';
 import { Dispatch, SetStateAction } from 'react';
 
 export interface ContextProps {
   snackbarState: SnackbarStateType;
   setSnackbarState: Dispatch<SetStateAction<SnackbarStateType>>;
-  registeringUser: UserType | null;
-  setRegisteringUser: Dispatch<SetStateAction<UserType | null>>;
-  selectedLocation: AddressType;
-  setSelectedLocation: Dispatch<SetStateAction<AddressType>>;
   isDarkModeOn: boolean;
   setIsDarkModeOn: Dispatch<SetStateAction<boolean>>;
   toggleTheme: () => void;
