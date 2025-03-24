4bf48cddcc6c548279db6d5c9303f5c4e109fa21
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-23 21:33:28 -0300

    refactor: optimize DatePicker component by wrapping it with memo for performance improvements

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,34 +1,36 @@
 import { TextField } from '@mui/material';
-import { ComponentProps } from 'react';
+import { ComponentProps, memo } from 'react';
 
 export interface DatePickerProps extends ComponentProps<'input'> {
   errors: any;
   register: any;
 }
 
-const DatePicker = ({
-  errors,
-  register,
-  required = false,
-  hidden,
-  ...rest
-}: DatePickerProps) => {
-  return (
-    !hidden && (
-      <TextField
-        id="birth-date"
-        label="Birth date"
-        fullWidth
-        type="date"
-        slotProps={{ inputLabel: { shrink: true } }}
-        {...register('birthDate')}
-        error={!!errors.birthDate}
-        helperText={errors.birthDate?.message?.toString()}
-        required={required}
-        {...rest}
-      />
-    )
-  );
-};
+const DatePicker = memo(
+  ({
+    errors,
+    register,
+    required = false,
+    hidden,
+    ...rest
+  }: DatePickerProps) => {
+    return (
+      !hidden && (
+        <TextField
+          id="birth-date"
+          label="Birth date"
+          fullWidth
+          type="date"
+          slotProps={{ inputLabel: { shrink: true } }}
+          {...register('birthDate')}
+          error={!!errors.birthDate}
+          helperText={errors.birthDate?.message?.toString()}
+          required={required}
+          {...rest}
+        />
+      )
+    );
+  },
+);
 
 export default DatePicker;
