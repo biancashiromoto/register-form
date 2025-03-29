5a3a9b501adda3a242e9858aa000a6ca36c5cdba
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-03-28 23:13:36 -0300

    feat: add reset password validation schema using zod

---
+++ src/schemas/resetPasswordSchema.ts
@@ -0,0 +1,17 @@
+import { passwordValidation } from '@/helpers';
+import { z } from 'zod';
+
+export const resetPasswordSchema = z
+  .object({
+    password: z
+      .string({ required_error: 'Password is required' })
+      .regex(passwordValidation, {
+        message:
+          'Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character',
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Password and confirm password must match',
+  });
