dd506478b54be88c4c4053f7b8531762e21c3648
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-13 12:33:24 -0300

    feat: restructure App component to include Outlet for nested routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import { Container } from '@mui/material';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import VerificationLayout from './components/VerificationLayout';
+import { Outlet } from '@tanstack/react-router';
 
-function App() {}
+function App() {
+  return (
+    <VerificationLayout>
+      <Header />
+      <Container>
+        <Outlet />
+      </Container>
+      <Footer />
+    </VerificationLayout>
+  );
+}
 
 export default App;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,51 +1,19 @@
-import CustomBottomNavigation from '@/components/BottomNavigation';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import VerificationLayout from '@/components/VerificationLayout';
-import { Context } from '@/context';
-import { useAuth } from '@/context/authContext';
 import Provider from '@/context/Provider';
-import usePageTitle from '@/hooks/usePageTitle';
-import {
-  Container,
-  CssBaseline,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
-import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { useContext } from 'react';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { createRootRoute } from '@tanstack/react-router';
+import App from '../App';
 
-const RootLayout = () => {
-  const { theme, isPrivateRoute } = useContext(Context);
-  const { page } = usePageTitle();
-  const { sessionRef } = useAuth();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header />
-      <hr />
-      <Container maxWidth="sm" sx={{ marginTop: '16px' }}>
-        {((isPrivateRoute && sessionRef) ||
-          (!isPrivateRoute && !sessionRef)) && (
-          <Typography variant="h5" align="left" gutterBottom>
-            {page?.title}
-          </Typography>
-        )}
-      </Container>
-      <Outlet />
-      {isPrivateRoute && sessionRef && <CustomBottomNavigation />}
-      <Footer />
-    </ThemeProvider>
-  );
-};
+const defaultTheme = createTheme();
 
 export const Route = createRootRoute({
-  component: () => (
-    <Provider>
-      <VerificationLayout>
-        <RootLayout />
-      </VerificationLayout>
-    </Provider>
-  ),
+  component: () => {
+    return (
+      <Provider>
+        <ThemeProvider theme={defaultTheme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    );
+  },
 });
