f7ebf14cc69edde1379b3b0b0f654891ff742480
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-17 21:40:56 -0300

    refactor: simplify RootLayout by removing unused components and improving structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,51 +1,34 @@
 import CustomBottomNavigation from '@/components/BottomNavigation';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import VerificationLayout from '@/components/VerificationLayout';
 import { Context } from '@/context';
 import { useAuth } from '@/context/authContext';
-import Provider from '@/context/Provider';
 import usePageTitle from '@/hooks/usePageTitle';
-import {
-  Container,
-  CssBaseline,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { useContext } from 'react';
 
 export const RootLayout = () => {
-  const { theme, isPrivateRoute } = useContext(Context);
+  const { isPrivateRoute } = useContext(Context);
   const { page } = usePageTitle();
   const { sessionRef } = useAuth();
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <>
       <Header />
       <hr />
-      <Container maxWidth="sm" sx={{ marginTop: '16px' }}>
-        {((isPrivateRoute && sessionRef) ||
-          (!isPrivateRoute && !sessionRef)) && (
-          <Typography variant="h5" align="left" gutterBottom>
-            {page?.title}
-          </Typography>
-        )}
+      <Container maxWidth="sm" sx={{ marginTop: '16px', flex: 1 }}>
+        <Typography variant="h5" align="left" gutterBottom>
+          {page?.title}
+        </Typography>
+        <Outlet />
       </Container>
-      <Outlet />
       {isPrivateRoute && sessionRef && <CustomBottomNavigation />}
       <Footer />
-    </ThemeProvider>
+    </>
   );
 };
 
 export const Route = createRootRoute({
-  component: () => (
-    <Provider>
-      <VerificationLayout>
-        <RootLayout />
-      </VerificationLayout>
-    </Provider>
-  ),
+  component: () => <RootLayout />,
 });
