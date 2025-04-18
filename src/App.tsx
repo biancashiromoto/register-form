06f9b0fb5b3fcbcf78c42f99f2b0d18ce5bcdacd
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-17 21:40:24 -0300

    refactor: implement context and routing logic in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import { Container } from '@mui/material';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import CustomBottomNavigation from '@/components/BottomNavigation';
+import { Container, Typography } from '@mui/material';
+import { useContext } from 'react';
 import VerificationLayout from './components/VerificationLayout';
+import { Context } from './context';
+import { useAuth } from './context/authContext';
+import Provider from './context/Provider';
+import usePageTitle from './hooks/usePageTitle';
 
-function App() {}
+const App = () => {
+  const { isPrivateRoute } = useContext(Context);
+  const { page } = usePageTitle();
+  const { sessionRef } = useAuth();
+
+  return (
+    <Provider>
+      <VerificationLayout>
+        <Container maxWidth="sm" sx={{ marginTop: '16px' }}>
+          {((isPrivateRoute && sessionRef) ||
+            (!isPrivateRoute && !sessionRef)) && (
+            <Typography variant="h5" align="left" gutterBottom>
+              {page?.title}
+            </Typography>
+          )}
+        </Container>
+        {isPrivateRoute && sessionRef && <CustomBottomNavigation />}
+      </VerificationLayout>
+    </Provider>
+  );
+};
 
 export default App;
