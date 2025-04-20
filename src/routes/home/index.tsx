2c7a0aa953b6190e9818739e52392a365b9ad861
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-20 11:48:20 -0300

    feat: implement authentication check in home route before loading component

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,17 +1,19 @@
-import { useAuth } from '@/context/authContext';
+import { isAuthenticated } from '@/services/user';
 import { Container, Typography } from '@mui/material';
-import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/home/')({
   component: RouteComponent,
+  beforeLoad: async () => {
+    const auth = await isAuthenticated();
+
+    if (!auth) {
+      throw redirect({ to: '/unauthenticated' });
+    }
+  },
 });
 
 export function RouteComponent() {
-  const { sessionRef } = useAuth();
-  const navigate = useNavigate();
-
-  if (!sessionRef) navigate({ to: '/unauthenticated' });
-
   return (
     <Container maxWidth="sm">
       <Typography variant="body2">You have successfully logged in!</Typography>
