39e2dfdd0848a1a4d6e997992d64188f3c91f91a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-21 18:12:46 -0300

    refactor: remove unused context and user update logic from useLoginUser hook

--- src/hooks/useLoginUser.tsx
+++ src/hooks/useLoginUser.tsx
@@ -1,48 +1,29 @@
-import { Context } from '@/context';
-import { useAuth } from '@/context/authContext';
 import { loginUser } from '@/services/user';
 import { UserType } from '@/types';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
-import { useContext } from 'react';
 import { UseFormSetError } from 'react-hook-form';
 
 type SetErrorFunction = UseFormSetError<{ email: string; password: string }>;
 
 const useLoginUser = (setError: SetErrorFunction) => {
   const navigate = useNavigate();
-  const { setUser, userRef } = useAuth();
-  const { setSnackbarState } = useContext(Context);
-
-  const waitForUserUpdate = (): Promise<void> => {
-    return new Promise((resolve) => {
-      const interval = setInterval(() => {
-        if (userRef.current !== null) {
-          clearInterval(interval);
-          resolve();
-        }
-      }, 50);
-    });
-  };
 
   const { isError, mutate, isPending } = useMutation({
     mutationKey: ['loginUser'],
-    mutationFn: async (credentials: Pick<UserType, 'email' | 'password'>) => {
-      const response = await loginUser(credentials);
-      if (!response || !response.data?.session) {
-        throw new Error('Invalid login credentials');
-      }
-      setUser(response.data.user);
-    },
+    mutationFn: async (credentials: Pick<UserType, 'email' | 'password'>) =>
+      await loginUser(credentials),
     onSuccess: async () => {
-      await waitForUserUpdate();
       navigate({ to: '/home', viewTransition: true });
     },
     onError: (error: any) => {
-      setSnackbarState({
-        open: true,
-        message: error.message,
-        severity: 'error',
+      setError('email', {
+        type: 'manual',
+        message: error.message || 'Incorrect email or password',
+      });
+      setError('password', {
+        type: 'manual',
+        message: error.message || 'Incorrect email or password',
       });
     },
   });
