252df7c6bfbcd9184edbc197770fa828578b9f37
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-21 18:06:18 -0300

    refactor: remove QueryClientProvider from main.tsx for simplified rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      gcTime: Infinity,
-    },
-  },
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
