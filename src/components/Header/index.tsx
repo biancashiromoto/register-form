01afd1cdc6f8fb93564db0a17a735b21b2a471a2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-04-23 20:25:51 -0300

    refactor: remove authentication and avatar logic from Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,11 @@
 import { Context } from '@/context';
-import { useAuth } from '@/context/authContext';
-import { Avatar, Box, Container, Skeleton, Typography } from '@mui/material';
-import { useNavigate } from '@tanstack/react-router';
+import { Box, Container } from '@mui/material';
 import { useContext } from 'react';
 import Navbar from '../Navbar';
 import ToggleThemeSwitch from '../ToggleThemeSwitch';
-import useAvatarUrl from '@/hooks/useAvatarUrl';
 
 const Header = () => {
   const { toggleTheme, isDarkModeOn } = useContext(Context);
-  const { sessionRef } = useAuth();
-  const navigate = useNavigate();
-  const { data: avatarUrl, isLoading: isLoadingAvatar } = useAvatarUrl();
 
   return (
     <Container>
@@ -24,38 +18,6 @@ const Header = () => {
           onChange={toggleTheme}
           checked={isDarkModeOn}
         />
-        {sessionRef && (
-          <Box
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              gap: 8,
-              position: 'absolute',
-              right: '75px',
-              transform: 'translateY(1px)',
-              cursor: 'pointer',
-            }}
-            onClick={() => navigate({ to: '/profile' })}
-          >
-            {!isLoadingAvatar ? (
-              <Avatar
-                src={avatarUrl || undefined}
-                sx={{
-                  width: 20,
-                  height: 20,
-                  border: '2px solid',
-                  borderColor: 'primary.main',
-                }}
-              />
-            ) : (
-              <Skeleton variant="circular" width={20} height={20} />
-            )}
-            <Typography variant="caption" color="textSecondary">
-              {sessionRef?.user?.email}
-            </Typography>
-          </Box>
-        )}
         <Navbar style={{ position: 'absolute', right: '12px' }} />
       </Box>
     </Container>
