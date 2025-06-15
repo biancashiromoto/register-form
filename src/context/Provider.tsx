3d93e3433888dec802eb5345982c139c8faa348f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-06-15 17:16:06 -0300

    refactor: improve avatar fetching logic and error handling

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -10,14 +10,35 @@ import ThemeProvider from './ThemeProvider';
 
 // const MAX_FILE_SIZE = 8 * 1024 * 1024; // 8MB
 
+/**
+ * Fetches the public URL of a user's avatar image from Supabase Storage.
+ *
+ * This function checks if the file `avatar.webp` exists in the user's folder within the
+ * 'avatars' storage bucket. If the file exists, it returns the public URL with a timestamp
+ * query parameter to prevent caching. If the file does not exist or an error occurs during
+ * the listing operation, it throws an error.
+ *
+ * @param session - The current user's session object containing user information.
+ * @returns A promise that resolves to the public URL of the user's avatar image.
+ * @throws Will throw an error if the avatar file is not found or if there is a listing error.
+ *
+ */
 const fetchAvatar = async (session: Session) => {
-  const { data, error } = await supabase.storage
+  const path = `${session.user.id}/avatar.webp`;
+
+  const { data: files, error: listError } = await supabase.storage
     .from('avatars')
-    .createSignedUrl(`${session?.user.id}/avatar.webp`, 1000);
+    .list(session.user.id, { search: 'avatar.webp' });
 
-  if (error) throw new Error(error.message);
+  const fileExists = files?.some((file) => file.name === 'avatar.webp');
 
-  return data;
+  if (listError || !fileExists) {
+    throw new Error('Avatar file not found');
+  }
+
+  const { data } = supabase.storage.from('avatars').getPublicUrl(path);
+
+  return `${data.publicUrl}?t=${Date.now()}`;
 };
 
 const convertToWebp = (file: File) => {
@@ -68,13 +89,14 @@ const uploadAvatar = async (file: File, session: Session) => {
 
   const { error } = await supabase.storage
     .from('avatars')
-    .update(`${session?.user.id}/avatar.webp`, convertedFile);
+    .upload(`${session.user.id}/avatar.webp`, convertedFile, {
+      upsert: true,
+    });
 
   if (error) throw new Error(error.message);
 
-  const { signedUrl } = await fetchAvatar(session);
-
-  return signedUrl ?? null;
+  const publicUrl = fetchAvatar(session);
+  return publicUrl;
 };
 
 const Provider: FC<{ children: ReactNode }> = ({ children }) => {
@@ -98,8 +120,8 @@ const Provider: FC<{ children: ReactNode }> = ({ children }) => {
     if (session) {
       setIsLoadingAvatar(true);
       const getAvatar = async () => {
-        const { signedUrl } = await fetchAvatar(session);
-        setAvatarPath(signedUrl ?? null);
+        const publicUrl = await fetchAvatar(session);
+        setAvatarPath(publicUrl ?? null);
       };
       getAvatar();
       setIsLoadingAvatar(false);
